1ad6f68598a0b7416f82ea16f386a8b407e41ef6
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-20 11:35:15 +0530

    Update running_sum_1D.py

--- Python/leetcode/running_sum_1D.py
+++ Python/leetcode/running_sum_1D.py
@@ -1,1 +1,9 @@
-
+def runningSum(nums):
+    n=len(nums)
+    prefix=[0]*n
+    for i in range(n):
+        if i==0:
+            prefix[i]=nums[i]
+        else:
+            prefix[i]=prefix[i-1]+nums[i]
+    return prefix
