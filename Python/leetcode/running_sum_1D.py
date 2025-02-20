a21fd1b9160692f7dd1446e7bf8efb048a189236
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-20 11:42:40 +0530

    Delete Python/leetcode directory

--- Python/leetcode/running_sum_1D.py
+++
@@ -1,9 +0,0 @@
-def runningSum(nums):
-    n=len(nums)
-    prefix=[0]*n
-    for i in range(n):
-        if i==0:
-            prefix[i]=nums[i]
-        else:
-            prefix[i]=prefix[i-1]+nums[i]
-    return prefix
