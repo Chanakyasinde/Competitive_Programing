db8f8fdb1c26e7bf220e7096e291f0acab61e88f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-21 17:48:21 +0530

    Update range_sum_query.py

--- Python/range_sum_query.py
+++ Python/range_sum_query.py
@@ -1,1 +1,15 @@
-
+arr=list(map(int,input().split()))
+n=len(arr)
+prefix=[0]*n
+for i in range(n):
+    if i==0:
+        prefix[i]=arr[i]
+    else:
+        prefix[i]=prefix[i-1]+arr[i]
+q=int(input())
+for _ in range(q):
+    l,r=map(int,input().split())
+    if l==0:
+        print(prefix[r])
+    else:
+        print(prefix[r]-prefix[l-1])
