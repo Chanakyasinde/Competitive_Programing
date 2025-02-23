634f833d35ca143426b824fd6813bd09539bca95
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-23 17:29:14 +0530

    Update max_sum_subarray_size_k.py

--- Python/leetcode/max_sum_subarray_size_k.py
+++ Python/leetcode/max_sum_subarray_size_k.py
@@ -1,1 +1,11 @@
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
 
+max_sum = current_sum = sum(a[:k])
+
+for i in range(k, n):
+    current_sum += a[i] - a[i - k]  
+    max_sum = max(max_sum, current_sum)
+
+
+print(max_sum)
