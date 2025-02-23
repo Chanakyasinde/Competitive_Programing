6945b738db241856c06c2af25ff5de5e5d31c0f5
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-23 17:28:37 +0530

    Update subarry_sum_divisible_by_k.py

--- Python/leetcode/subarry_sum_divisible_by_k.py
+++ Python/leetcode/subarry_sum_divisible_by_k.py
@@ -1,1 +1,24 @@
+n, k = map(int, input().split())
+nums = list(map(int, input().split()))
 
+prefix_sum = 0
+count = 0
+remainder_count = {}  
+remainder_count[0] = 1  
+
+for num in nums:
+    prefix_sum += num
+    remainder = prefix_sum % k
+    
+    if remainder < 0:
+        remainder += k
+    
+    if remainder in remainder_count:
+        count += remainder_count[remainder]
+    
+    if remainder in remainder_count:
+        remainder_count[remainder] += 1
+    else:
+        remainder_count[remainder] = 1
+
+print(count)
