0012256ded19b70ff3a6b0d1a3f07ca40bcb29d1
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-08 23:41:03 +0530

    Update diagonal_sum.py

--- Python/leetcode/diagonal_sum.py
+++ Python/leetcode/diagonal_sum.py
@@ -1,1 +1,9 @@
-
+def calculate_diagonal_sum(matrix):
+    n=len(matrix)
+    total=0
+    for i in range(n):
+        total+=matrix[i][i]
+        total+=matrix[i][n-1-i]
+        if i==n-1-i:
+            total-=matrix[i][i]
+    return total
