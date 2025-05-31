89fff011e72db142698822170413ceff4385cd82
Chanakya Sinde <chanakyasinde@gmail.com>
2025-05-31 16:39:24 +0530

    Update fav_sequence.py

--- Python/leetcode/fav_sequence.py
+++ Python/leetcode/fav_sequence.py
@@ -1,1 +1,14 @@
-
+q=int(input())
+for _ in range(q):
+    n=int(input())
+    lst=list(map(int,input().split()))
+    new=[0]*n
+    x,y=0,n-1
+    for i in range(n):
+        if i%2==0:
+            new[i]=lst[x]
+            x+=1
+        else:
+            new[i]=lst[y]
+            y-=1
+    print(*new)
