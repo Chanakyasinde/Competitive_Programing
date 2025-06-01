48c30eb6fedee2ed3bc853940f72411aaaafbcdf
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-01 17:15:50 +0530

    Update sum_odd_len_subarr.py

--- Python/leetcode/sum_odd_len_subarr.py
+++ Python/leetcode/sum_odd_len_subarr.py
@@ -1,1 +1,10 @@
+def sumOddLengthSubarrays(arr):
+    n = len(arr)
+    count = 0
 
+    for i in range(n):
+        left = i+1
+        right = n-i
+        odd_count = ((left+1)//2)*((right+1)//2) + ((left//2)*(right//2))
+        count += arr[i]*odd_count
+    return count
