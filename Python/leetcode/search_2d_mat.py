470ffd14c00345a43eef3c5fc0a12d8bfe51564d
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-02 20:38:53 +0530

    Update search_2d_mat.py

--- Python/leetcode/search_2d_mat.py
+++ Python/leetcode/search_2d_mat.py
@@ -1,1 +1,17 @@
-
+def searchMatrix(mat, target):
+    n = len(mat)
+    m = len(mat[0])
+    low, high = 0, n * m - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        mid_value = mat[mid // m][mid % m]
+        
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return False
