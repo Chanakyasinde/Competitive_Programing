bf282c51dbb2dc527edf6b52c219fd531549be6f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-03 14:58:14 +0530

    Update climbing_stairs_recursion.py

--- Python/leetcode/climbing_stairs_recursion.py
+++ Python/leetcode/climbing_stairs_recursion.py
@@ -1,1 +1,12 @@
+def recursion(arr,n):
+    if arr[n]!=-1:
+        return arr[n]
+    if n==0 or n==1:
+        return 1
+    else:
+        arr[n] = recursion(arr,n-1)+recursion(arr,n-2)
+        return arr[n]
 
+def climbStairs(n):
+    arr=[-1]*(n+1)
+    return recursion(arr,n)
