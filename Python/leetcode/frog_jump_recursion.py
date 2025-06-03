59c396b97fafa0a94b19e853f2ea5eee066af0c5
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-03 14:57:05 +0530

    Update frog_jump_recursion.py

--- Python/leetcode/frog_jump_recursion.py
+++ Python/leetcode/frog_jump_recursion.py
@@ -1,1 +1,18 @@
-
+def frogJump(heights):
+    n = len(heights)
+    dp = [-1] * n
+    
+    def recursive(index):
+        if index == n - 1:
+            return 0
+        if dp[index] != -1:
+            return dp[index]
+        energy = float('inf')
+        if index + 1 < n:
+            energy = min(energy, abs(heights[index] - heights[index + 1]) + recursive(index + 1))
+        if index + 2 < n:
+            energy = min(energy, abs(heights[index] - heights[index + 2]) + recursive(index + 2))
+        dp[index] = energy
+        return dp[index]
+    
+    return recursive(0)
