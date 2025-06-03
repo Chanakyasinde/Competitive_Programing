ca4cf70859c0ae4e4746855e5eaae5d18ffe92e7
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-03 14:55:14 +0530

    Update int_roman_conversion.py

--- Python/leetcode/int_roman_conversion.py
+++ Python/leetcode/int_roman_conversion.py
@@ -1,1 +1,15 @@
-
+def int_to_roman(num):
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    
+    result = ""
+    for i in range(len(values)):
+        value = values[i]
+        symbol = symbols[i]
+        while num >= value:
+            result += symbol
+            num -= value
+    
+    return result
+num=int(input())
+print(int_to_roman(num))
