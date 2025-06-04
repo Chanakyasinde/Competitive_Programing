8ed161be4b1ba90ec6721043f5968bf7c6f96bc6
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-04 19:20:37 +0530

    Update inversion_bubble_sort.py

--- Python/leetcode/inversion_bubble_sort.py
+++ Python/leetcode/inversion_bubble_sort.py
@@ -1,1 +1,9 @@
-
+def count_inversions(list_of_numbers):
+    n=len(list_of_numbers)
+    count=0
+    for j in range(n-1):
+        for i in range(1,n):
+            if list_of_numbers[i-1]>list_of_numbers[i]:
+                list_of_numbers[i-1],list_of_numbers[i]=list_of_numbers[i],list_of_numbers[i-1]
+                count+=1
+    return count
