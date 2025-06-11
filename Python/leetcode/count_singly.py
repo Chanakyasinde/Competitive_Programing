078682f829492a80b98a7f04e95b7d9a3bd82a36
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-11 08:30:07 +0530

    Update count_singly.py

--- Python/leetcode/count_singly.py
+++ Python/leetcode/count_singly.py
@@ -1,1 +1,15 @@
+'''
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+'''
 
+def count_occurrences(head, K):
+    current = head
+    count = 0
+    while current is not None:
+        if current.data == K:
+            count+=1
+        current = current.next
+    return count
